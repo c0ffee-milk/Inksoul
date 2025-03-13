0a6f797794cbea6a17c5fecbdcd9a9a0934a9f6f
captain798 <167271814+captain798@users.noreply.github.com>
2025-03-13 22:24:28 +0800

    初步新增了日记的增删改，forms待完善

--- routes/diary.py
+++ routes/diary.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_pagination import Pagination
-from models import DiaryModel
+from models import DiaryModel, UserModel
 from flask_login import current_user, login_required
 from exts import db
 bp = Blueprint('diary', __name__, url_prefix='/diary')
@@ -28,6 +28,44 @@ def add():
             diary = DiaryModel(title=title, content=content, user_id=current_user.id)
             db.session.add(diary)
             db.session.commit()
+    # 更新用户的 diary_count
+            user = UserModel.query.get(current_user.id)
+            user.diary_count += 1
+            db.session.commit()
             flash('日记添加成功', 'success')
             return redirect(url_for('index'))
     return render_template('index.html')
+
+@bp.route('/edit/<int:diary_id>', methods=['GET', 'POST'])
+@login_required
+def edit(diary_id):
+    diary = DiaryModel.query.get(diary_id)
+    if diary.author_id == current_user.id:
+        if request.method == 'POST':
+            title = request.form['title']
+            content = request.form['content']
+            if not title or not content:
+                return redirect(url_for('diary.edit', diary_id=diary_id))
+            else:
+                diary.title = title
+                diary.content = content
+                db.session.commit()
+                return redirect(url_for('index'))
+        return render_template('edit.html', diary=diary)
+    else:
+        return redirect(url_for('index'))
+
+@bp.route('/delete/<int:diary_id>', methods=['GET', 'POST'])
+@login_required
+def delete(diary_id):
+    diary = DiaryModel.query.get(diary_id)
+    if diary.author_id == current_user.id:
+        db.session.delete(diary)
+        db.session.commit()
+        # 更新用户的 diary_count
+        user = UserModel.query.get(current_user.id)
+        user.diary_count -= 1
+        db.session.commit()
+        return redirect(url_for('index'))
+    else:
+        return redirect(url_for('index'))
