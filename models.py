e7df3d7ad5ec91ccc12be7abc67773e6ea9d7312
captain798 <167271814+captain798@users.noreply.github.com>
2025-03-13 16:14:23 +0800

    更新

--- models.py
+++ models.py
@@ -26,4 +26,3 @@ class DiaryModel(db.Model):
     content = db.Column(db.Text, nullable=False)
     create_time = db.Column(db.DateTime, default=datetime.now)
     author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    author = db.relationship('UserModel', backref=db.backref('diaries'))
