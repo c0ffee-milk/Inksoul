e7890eaa7bfc2e84455e1eaa3d5f3c4ded2ba850
captain798 <167271814+captain798@users.noreply.github.com>
2025-03-13 18:44:50 +0800

    实现填写日记功能，修改根目录

--- routes/diary.py
+++ routes/diary.py
@@ -1,8 +1,33 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
-
-bp = Blueprint('diary', __name__)
+from flask_pagination import Pagination
+from models import DiaryModel
+from flask_login import current_user, login_required
+from exts import db
+bp = Blueprint('diary', __name__, url_prefix='/diary')
 
 @bp.route('/')
 def index():
-    return render_template('index.html')
+    if current_user.is_authenticated:
+        # 获取当前用户的所有日记，按创建时间倒序排列
+        diaries = DiaryModel.query.filter_by(author_id=current_user.id).order_by(DiaryModel.create_time.desc()).all()
+    else:
+        return redirect(url_for('auth.login'))
+    return render_template('index.html', diaries=diaries)
 
+@bp.route('/add', methods=['GET', 'POST'])
+@login_required
+def add():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        if not title or not content:
+            flash('日记和标题不能为空')
+            return redirect(url_for('diary.add'))
+        else:
+            # 创建日记并保存到数据库
+            diary = DiaryModel(title=title, content=content, user_id=current_user.id)
+            db.session.add(diary)
+            db.session.commit()
+            flash('日记添加成功', 'success')
+            return redirect(url_for('index'))
+    return render_template('index.html')
