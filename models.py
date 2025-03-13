dc322faba1f5afe686dfb167c8d6002b0ba86307
captain798 <167271814+captain798@users.noreply.github.com>
2025-03-13 14:15:53 +0800

    数据库模型函数

---
+++ models.py
@@ -0,0 +1,29 @@
+from exts import db
+from datetime import datetime
+
+# 用户模型
+class UserModel(db.Model):
+    __tablename__ = "user"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(1000), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    join_time = db.Column(db.DateTime, default=datetime.now)
+    diaries = db.relationship('DiaryModel', backref='author', lazy='dynamic')
+
+# 邮箱验证码模型
+class EmailCaptchaModel(db.Model):
+    __tablename__ = "email_captcha"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String(100), nullable=False)
+    captcha = db.Column(db.String(100), nullable=False)
+
+# 日记模型
+class DiaryModel(db.Model):
+    __tablename__ = "diary"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    create_time = db.Column(db.DateTime, default=datetime.now)
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    author = db.relationship('UserModel', backref=db.backref('diaries'))
