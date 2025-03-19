de927b310874daf1285d9fb48c687af420cbacd9
captain798 <2838651487@qq.com>
2025-03-19 16:30:40 +0800

    登陆后直接跳转日记页面

--- routes/index.py
+++ routes/index.py
@@ -1,7 +1,9 @@
-from flask import render_template,Blueprint
-
+from flask import render_template,Blueprint,redirect,url_for
+from flask_login import current_user
 bp = Blueprint('index', __name__,url_prefix="/")
 
 @bp.route('/')
 def index_page():
+    if current_user.is_authenticated:
+        return redirect(url_for('diary.mine'))
     return render_template('index.html')
